db308538978e88592235e8dfb6360bf8b3db65ee
LucianoASF <lucianoaugustos12@gmail.com>
2026-05-02 16:47:46 -0300

    fix(users): add missing update user dto

---
+++ SistemaTicket/Dtos/ApplicationUser/ApplicationUserUpdateDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaTicket.Dtos.ApplicationUser;
+
+public class ApplicationUserUpdateDto
+{
+    [Required]
+    [StringLength(100, MinimumLength = 5)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    [StringLength(256)]
+    public string Email { get; set; } = string.Empty;
+
+    [StringLength(60, MinimumLength = 6)]
+    public string? Password { get; set; }
+
+    public List<string>? Roles { get; set; }
+
+}
