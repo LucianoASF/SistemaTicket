b3e616e9f3a7ffcd524c5bb99e109f6df4627378
LucianoASF <lucianoaugustos12@gmail.com>
2026-05-08 07:39:02 -0300

    feat: install and configuration react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-import { Button } from '#components/ui/button';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import { Login } from './pages/Login';
 
-function App() {
-  return <Button variant={'destructive'}>Olá mundo!</Button>;
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App;
