da8366ea017404e13161a353e4213f8723424401
LucianoASF <lucianoaugustos12@gmail.com>
2026-05-20 14:33:55 -0300

    feat: implement user login flow

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '#components/ui/card';
@@ -11,9 +10,45 @@ import { Input } from '#components/ui/input';
 import { Label } from '#components/ui/label';
 import { Eye, EyeOff, Ticket } from 'lucide-react';
 import { useState } from 'react';
+import { useAuth } from '../contexts/useAuth';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Field, FieldError, FieldGroup } from '#components/ui/field';
+
+const loginSchema = z.object({
+  email: z
+    .email('Digite um email válido')
+    .max(256, 'Email deve ter no máximo 256 caracteres'),
+  password: z
+    .string()
+    .min(6, 'Senha deve ter pelo menos 6 caracteres')
+    .max(60, 'Senha deve ter no máximo 60 caracteres')
+    .regex(/[A-Z]/, 'A senha deve ter pelo menos uma letra maiúscula')
+    .regex(/[a-z]/, 'A senha deve ter pelo menos uma letra minúscula')
+    .regex(/[0-9]/, 'A senha deve ter pelo menos um número')
+    .regex(/[^A-Za-z0-9]/, 'A senha deve ter pelo menos um caractere especial'),
+});
+
+type LoginFormInputs = z.infer<typeof loginSchema>;
 
 export function Login() {
+  const { login } = useAuth();
   const [showPassword, setShowPassord] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  if (isSubmitting) console.log(isSubmitting);
+
+  const onSubmit = async (data: LoginFormInputs) => {
+    await login(data.email, data.password);
+  };
 
   return (
     <main className="h-screen flex items-center justify-center">
@@ -30,49 +65,56 @@ export function Login() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="seu@email.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">Senha</Label>
-                <div className="relative">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <fieldset>
+              <FieldGroup className="flex flex-col gap-6">
+                <Field className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
                   <Input
-                    id="password"
-                    type={showPassword ? 'text' : 'password'}
-                    placeholder="Sua senha"
-                    required
+                    id="email"
+                    type="email"
+                    placeholder="seu@email.com"
+                    autoComplete="email"
+                    {...register('email')}
                   />
-                  <Button
-                    type="button"
-                    variant="ghost"
-                    size="icon"
-                    className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
-                    onClick={() => setShowPassord((prev) => !prev)}
-                  >
-                    {showPassword ? (
-                      <EyeOff className="h-4 w-4 text-muted-foreground" />
-                    ) : (
-                      <Eye className="h-4 w-4 text-muted-foreground" />
-                    )}
-                  </Button>
-                </div>
-              </div>
-            </div>
+                  <FieldError>{errors.email?.message}</FieldError>
+                </Field>
+                <Field className="grid gap-2">
+                  <Label htmlFor="password">Senha</Label>
+                  <div className="relative">
+                    <Input
+                      id="password"
+                      type={showPassword ? 'text' : 'password'}
+                      placeholder="Sua senha"
+                      {...register('password')}
+                    />
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="icon"
+                      className="absolute top-0 right-0 h-full px-3 hover:bg-transparent"
+                      onClick={() => setShowPassord((prev) => !prev)}
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-4 w-4 text-muted-foreground" />
+                      ) : (
+                        <Eye className="h-4 w-4 text-muted-foreground" />
+                      )}
+                    </Button>
+                  </div>
+                  <FieldError>{errors.password?.message}</FieldError>
+                </Field>
+              </FieldGroup>
+            </fieldset>
+            <Button
+              type="submit"
+              className="mt-6 w-full"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? 'Entrando...' : 'Login'}
+            </Button>
           </form>
         </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </CardFooter>
       </Card>
     </main>
   );
