a8d14ce25d9b827a5825b5519faeabfe0d15c588
LucianoASF <lucianoaugustos12@gmail.com>
2026-05-22 21:18:35 -0300

    feat: add ticket types

---
+++ frontend/src/types/ticket.ts
@@ -0,0 +1,26 @@
+export const TICKET_STATUS = {
+  OPEN: 'Open',
+  INPROGRESS: 'InProgress',
+  CLOSED: 'Closed',
+} as const;
+
+export type TicketStatus = (typeof TICKET_STATUS)[keyof typeof TICKET_STATUS];
+
+export const TICKET_PRIORITY = {
+  LOW: 'Low',
+  MEDIUM: 'Medium',
+  HIGH: 'High',
+} as const;
+
+export type TicketPriority =
+  (typeof TICKET_PRIORITY)[keyof typeof TICKET_PRIORITY];
+
+export interface Ticket {
+  id: number;
+  title: string;
+  description: string;
+  status: TicketStatus;
+  priority: TicketPriority;
+  createdAt: Date;
+  createdById: string;
+}
