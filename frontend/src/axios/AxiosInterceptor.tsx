c2dac504c51a9bb3c6d56659d6845ec720689b1f
LucianoASF <lucianoaugustos12@gmail.com>
2026-07-05 15:48:46 -0300

    fix: remove duplicate display from error toast

--- frontend/src/axios/AxiosInterceptor.tsx
+++ frontend/src/axios/AxiosInterceptor.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useAuth } from '../contexts/useAuth';
 import { api } from './axios';
 import { toast } from 'sonner';
@@ -7,31 +8,40 @@ interface AxiosInterceptorProps {
 }
 
 export function AxiosInterceptor({ children }: AxiosInterceptorProps) {
+  console.log('aaaaaaaaaaaaaaaaaaaaaaaa');
   const { setUser } = useAuth();
 
-  api.interceptors.response.use(
-    (res) => res,
-    (error) => {
-      if (error.config.url === '/auth/me' && error.response?.status === 401) {
-        setUser(null);
-      } else if (error.response?.status === 401) {
-        setUser(null);
-        toast.error(error.response?.data?.message || 'Erro inesperado', {
-          position: 'top-right',
-        });
-      } else if (
-        !(
-          (error.response?.status === 403 || error.response?.status === 404) &&
-          error.config.skip403And404Toast
-        )
-      ) {
-        toast.error(error.response?.data?.message || 'Erro inesperado', {
-          position: 'top-right',
-        });
-      }
-      return Promise.reject(error);
-    },
-  );
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      (res) => res,
+      (error) => {
+        if (error.config.url === '/auth/me' && error.response?.status === 401) {
+          setUser(null);
+        } else if (error.response?.status === 401) {
+          setUser(null);
+          toast.error(error.response?.data?.message || 'Erro inesperado', {
+            position: 'top-right',
+          });
+        } else if (
+          !(
+            (error.response?.status === 403 ||
+              error.response?.status === 404) &&
+            error.config.skip403And404Toast
+          )
+        ) {
+          toast.error(error.response?.data?.message || 'Erro inesperado', {
+            position: 'top-right',
+          });
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      api.interceptors.response.eject(interceptorId);
+    };
+  }, [setUser]);
 
   return children;
 }
